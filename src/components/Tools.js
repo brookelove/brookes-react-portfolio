2150c619a749739a8b2bf65574273e87dd445fe2
Brooke Love <savienloveb@gmail.com>
2022-05-29 13:48:16 -0700

    creating wrapping affect on tools and skills

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { DiGithubBadge, DiVisualstudio, DiHtml5, DiCss3, DiBootstrap, DiJqueryLogo, DiNodejsSmall,DiResponsive} from "react-icons/di";
+import { DiGithubBadge, DiVisualstudio, DiHtml5, DiCss3, DiBootstrap, DiJqueryLogo, DiNodejsSmall, DiReact} from "react-icons/di";
+import { MdOutlineDevicesOther } from "react-icons/md";
+import { IconContext } from 'react-icons';
 import '../styles/Tools.css';
 
 
@@ -10,44 +12,48 @@ export default function Tools () {
          <h4> The Tools & Skills I have learned so far...</h4>
         <div className='gridContainer'>
             <div class="grid_item">
-                <DiGithubBadge size="6em"/>
+                <DiGithubBadge size="5em"/>
                 <p>Github</p>
             </div>
             <div class="grid_item">
-                <DiVisualstudio size="6em"/>
+                <DiVisualstudio size="5em"/>
                 <p>VSCODE</p>
             </div>
             <div class="grid_item">
-                <DiHtml5 size="6em"/>
+                <DiHtml5 size="5em"/>
                 <p>HTML5</p>
             </div>
             <div class="grid_item">
-                <DiResponsive size="6em"/>
+                <MdOutlineDevicesOther size="5em"/>
                 <p>Responsive Website</p>
             </div>
             <div class="grid_item">
-                <DiCss3 size="6em"/>
+                <DiCss3 size="5em"/>
                 <p>CSS3</p>
             </div>
             <div class="grid_item">
-                <DiCss3 size="6em"/>
+                <DiCss3 size="5em"/>
                 <p>JS</p>
             </div>
             <div class="grid_item">
-                <DiBootstrap size="6em"/>
+                <DiBootstrap size="5em"/>
                 <p>Bootstrap</p>
             </div>
             <div class="grid_item">
-                <DiJqueryLogo size="6em"/>
+                <DiJqueryLogo size="5em"/>
                 <p>Jquery</p>
             </div>
             <div class="grid_item">
-                <DiNodejsSmall size="6em"/>
-                <p>nodejs</p>
+                <DiNodejsSmall size="5em"/>
+                <p>Nodejs</p>
+            </div>
+            <div class="grid_item">
+                <DiReact size="5em"/>
+                <p>React</p>
             </div>
         </div>
+        {/* have to figure out why there is no space here */}
         <h4> I am currently learning <u>React</u>.</h4>
      </div> 
-
     )
 }
