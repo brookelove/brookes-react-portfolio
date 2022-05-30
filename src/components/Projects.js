87f191259bfea70a589b75975d1ec149b9ff1a28
Brooke Love <savienloveb@gmail.com>
2022-05-30 15:14:01 -0700

    creating initial look of cards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,27 +8,54 @@ export default function Navbar () {
             <h4> Here are a few of the projects I have worked on</h4>
             <div className="container">
                 <div className="card">
-                    <div className="card_img"></div>
-                    <h2>Title</h2>
-                    <p>Text</p>
+                    <div className="card_img card-1"></div>
+                    <h2 className="cardTitle">Project 1</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     <ul>
                         <li><a>Website</a></li>
                         <li><a>Github</a></li>
                     </ul>
                 </div>
                 <div className="card">
-                    <div className="card_img"></div>
-                    <h2>Title</h2>
-                    <p>Text</p>
+                    <div className="card_img card-2"></div>
+                    <h2 className="cardTitle">Title</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     <ul>
                         <li><a>Website</a></li>
                         <li><a>Github</a></li>
                     </ul>
                 </div>
                 <div className="card">
-                    <div className="card_img"></div>
-                    <h2>Title</h2>
-                    <p>Text</p>
+                    <div className="card_img card-3"></div>
+                    <h2 className="cardTitle">Title</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <ul>
+                        <li><a>Website</a></li>
+                        <li><a>Github</a></li>
+                    </ul>
+                </div>
+                <div className="card">
+                    <div className="card_img card-4"></div>
+                    <h2 className="cardTitle">Title</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <ul>
+                        <li><a>Website</a></li>
+                        <li><a>Github</a></li>
+                    </ul>
+                </div>
+                <div className="card">
+                    <div className="card_img card-5"></div>
+                    <h2 className="cardTitle">Title</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <ul>
+                        <li><a>Website</a></li>
+                        <li><a>Github</a></li>
+                    </ul>
+                </div>
+                <div className="card">
+                    <div className="card_img card-6"></div>
+                    <h2 className="cardTitle">Title</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     <ul>
                         <li><a>Website</a></li>
                         <li><a>Github</a></li>
